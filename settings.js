79bd73d2a9f9b5f849a5460106a424551623e5ae
admin <admin@James-Vreekens-Mac-Pro.local>
2018-09-04 14:52:21 -0600

    Added template id

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ exports.listId = 5036793;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = null;
+exports.templateId = c20e23d1-2b38-49d8-b825-f9028b99c516;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
